0fc996868504b3fa7c86a0cb32a82b7246fcb705
AKramAtiia <akramatiia@gmail.com>
2026-09-06 07:12:18 +0300

    fix(web): switch Supabase URL to active secure HTTPS Cloudflare Tunnel endpoint

--- lib/core/supabase_config.dart
+++ lib/core/supabase_config.dart
@@ -1,7 +1,7 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AdminSupabaseConfig {
-  static const String supabaseUrl = 'http://178.105.236.62:8000';
+  static const String supabaseUrl = 'https://incredible-network-traveler-fiber.trycloudflare.com';
   static const String supabaseAnonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzg3NjE5Njg5LCJleHAiOjIxMDI5Nzk2ODl9.dEfM6Hzs_QiVgqBqjWa3kbsrwKMauHemBB8IvrKwgRM';
 
